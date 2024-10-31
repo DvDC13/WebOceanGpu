37076e7c30592e3dc92fa165e2b53f18d0336612
DvDC13 <davidchemaly1@gmail.com>
2024-10-31 19:06:20 +0100

    feat(test shaders) : addes glsl shaders for testing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as BABYLON from "@babylonjs/core";
+import vertShader from "./shaders/vert.glsl";
+import fragShader from "./shaders/frag.glsl";
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
@@ -11,8 +13,25 @@ const scene = new BABYLON.Scene(engine);
 const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, 0, 0), scene);
 camera.attachControl(canvas, true);
 
-const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
-BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+const light = new BABYLON.PointLight("light", new BABYLON.Vector3(5, 5, 5), scene);
+
+const box = BABYLON.MeshBuilder.CreateBox("box", { width: 1, height: 1, depth: 1 }, scene);
+
+BABYLON.Effect.ShadersStore["customVertexShader"] = vertShader;
+BABYLON.Effect.ShadersStore["customFragmentShader"] = fragShader;
+
+const shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
+  vertex: "custom",
+  fragment: "custom",
+},
+{
+  attributes: ["position"],
+  uniforms: ["worldViewProjection"],
+});
+
+const mainTexture = new BABYLON.Texture("./assets/tex_test.jpeg", scene);
+shaderMaterial.setTexture("textureSampler", mainTexture);
+box.material = shaderMaterial;
 
 engine.runRenderLoop(() => {
   scene.render();
---
+++ src/shaders/vert.glsl
@@ -0,0 +1,11 @@
+// vert.glsl
+precision highp float;
+
+// Attributes
+attribute vec3 position;
+
+uniform mat4 worldViewProjection;
+
+void main() {
+    gl_Position = worldViewProjection * vec4(position, 1.0);
+}
